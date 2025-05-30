e434734ade37ba3604c18508aa8012132de48907
vrbalbudhe <vrbalbudhe@gmail.com>
2025-05-30 20:07:01 +0530

    added the report and archives pages for the user

--- Frontend/src/pages/ProfilePage.jsx
+++ Frontend/src/pages/ProfilePage.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import ProfileSidebar from "../components/profile/ProfileSidebar";
+import ReportSection from "../components/profile/report/ReportSection";
+import ProfileMainPage from "../components/profile/home/ProfileMainPage";
+import ReportsArchive from "../components/profile/archives/ReportsArchive";
 
 function ProfilePage() {
   const { id } = useParams();
-
+  const [selectedIcon, setSelectedIcon] = useState("Home");
   return (
     <div className="w-full min-h-screen overflow-x-hidden">
-      
+      <div className="w-full flex">
+        <div className="min-w-fit">
+          <ProfileSidebar
+            selectedIcon={selectedIcon}
+            setSelectedIcon={setSelectedIcon}
+          />
+        </div>
+        {selectedIcon === "Home" && <ProfileMainPage />}
+        {selectedIcon === "Report" && <ReportSection />}
+        {selectedIcon === "Archives" && <ReportsArchive />}
+      </div>
     </div>
   );
 }
